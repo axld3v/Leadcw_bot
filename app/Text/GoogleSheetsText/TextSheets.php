fad2fd390700ea6d411177fd10116610f9679eed
axldev <axld3v@gmail.com>
2025-09-09 13:36:24 +0700

    add parse sheets

--- app/Text/GoogleSheetsText/TextSheets.php
+++ app/Text/GoogleSheetsText/TextSheets.php
@@ -12,51 +12,29 @@ class TextSheets
             $text_arr = $sheets->getValueCells("Текста", "B3:Z");
 
             $json = [
-                'date_last_update'   => date("Y-m-d H:i:s"),
-                'start'              => $text_arr[0][0],
-                'who'                => [
-                    'text'  => $text_arr[1][0] ?? "",
-                    'items' => self::clearItems($text_arr[2] ?? [])
+                'date_last_update' => date("Y-m-d H:i:s"),
+                'start'            => $text_arr[0][0],
+                'date'             => [
+                    'text'  => $text_arr[2][0] ?? "",
+                    'items' => self::clearItems($text_arr[3] ?? [])
                 ],
-                'name'               => ['text' => $text_arr[4][0] ?? "",],
-                'phone'              => ['text' => $text_arr[5][0] ?? "",],
-                'typecontact'       => [
-                    'text'  => $text_arr[7][0] ?? "",
-                    'items' => self::clearItems($text_arr[8] ?? [])
+                'requests_tg'      => [
+                    'text'  => $text_arr[5][0] ?? "",
+                    'items' => self::clearItems($text_arr[6] ?? [])
                 ],
-                'source'             => [
-                    'text'  => $text_arr[10][0] ?? "",
-                    'items' => self::clearItems($text_arr[11] ?? [])
+                'requests_inst'    => [
+                    'text'  => $text_arr[8][0] ?? "",
+                    'items' => self::clearItems($text_arr[9] ?? [])
                 ],
-                'brand'              => [
-                    'text'  => $text_arr[13][0] ?? "",
-                    'items' => self::clearItems($text_arr[14] ?? [])
+                'requests_wat'     => [
+                    'text'  => $text_arr[11][0] ?? "",
+                    'items' => self::clearItems($text_arr[12] ?? [])
                 ],
-                'complexion'         => ['text' => $text_arr[16][0] ?? "",],
-                'agent'              => [
-                    'text'  => $text_arr[18][0] ?? "",
-                    'items' => self::clearItems($text_arr[19] ?? [])
+                'requests_all'     => [
+                    'text'  => $text_arr[14][0] ?? "",
+                    'items' => self::clearItems($text_arr[15] ?? [])
                 ],
-                'price'              => ['text' => $text_arr[21][0] ?? "",],
-                'prepayment'         => [
-                    'text'  => $text_arr[23][0] ?? "",
-                    'items' => self::clearItems($text_arr[24] ?? [])
-                ],
-                'prepaymentget'     => ['text' => $text_arr[26][0] ?? "",],
-                'city'               => [
-                    'text'  => $text_arr[28][0] ?? "",
-                    'items' => self::clearItems($text_arr[29] ?? [])
-                ],
-                'type'               => [
-                    'text'  => $text_arr[31][0] ?? "",
-                    'items' => self::clearItems($text_arr[32] ?? [])
-                ],
-                'additionalcomment' => [
-                    'text'  => $text_arr[34][0] ?? "",
-                ],
-                'zahod' => [
-                    'text'  => $text_arr[36][0] ?? "",
-                ],
+                'check'            => $text_arr[17][0] ?? "",
             ];
             $jsonData = json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
             $ok = file_put_contents(storage_path('text/text.json'), $jsonData);
